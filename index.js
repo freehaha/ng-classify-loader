aace77f09b00af8215d95d497a75472231b165cc
Freehaha <freehaha@gmail.com>
2016-01-08 20:57:43 +1100

    typo fix #1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var classify = require('ng-classify')
 
 module.exports = function(content) {
-  this.casheable();
+  this.cacheable();
   var options = this.options || {};
   if(options.ngClassify) {
     options = options.ngClassify;
